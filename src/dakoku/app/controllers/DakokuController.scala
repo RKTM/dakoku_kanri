2541a93575308322aa3bc18653a621b4389c0279
RKTM <rrrkitamura@gmail.com>
2012-03-18 00:09:12 +0900

    register geo data.

--- src/dakoku/app/controllers/DakokuController.scala
+++ src/dakoku/app/controllers/DakokuController.scala
@@ -9,17 +9,16 @@ import models.Employee
 import models.Dakoku
 import anorm.NotAssigned
 import java.util.Date
+import utils.Geo
 
 object DakokuController extends Controller {
 
   val dakokuForm = Form(
-      tuple(
+    tuple(
       "employee_cd" -> nonEmptyText,
-      "dummy" -> text
-      ) verifying ("Invalid Employee Cd", result => result match {
+      "dummy" -> text) verifying ("Invalid Employee Cd", result => result match {
         case (employee_cd, _) => Employee.findByCd(employee_cd).isDefined
-        })
-      )
+      }))
 
   def index = Action { implicit request =>
     Ok(views.html.dakoku.index(DakokuSelect.findAll(), dakokuForm))
@@ -32,11 +31,11 @@ object DakokuController extends Controller {
         case (employee_cd, _) =>
           val employee = Employee.findByCd(employee_cd)
           employee match {
-            case Some(e) => 
+            case Some(e) =>
               Dakoku.create(Dakoku(id = NotAssigned, employee_id = e.id.get,
-                  start_work_at = new Date, finish_work_at = null));
+                start_work_at = new Date, finish_work_at = null, start_work_geo = Option(Geo(1d, 2d))));
               Redirect(routes.DakokuController.index)
-            case None => 
+            case None =>
               NotFound("NO_DATA")
           }
       })
